cb602cd56179e72064327d1fcd0b6aa99b645c1a
ushiboy <ushiboy.dev@gmail.com>
2019-11-03 22:42:59 +0900

    add docker script

--- case2/test/environment.py
+++ case2/test/environment.py
@@ -1,4 +1,7 @@
 import yaml
+import subprocess
+import os
+
 
 def load_e2e_config(filepath):
     with open(filepath, 'r') as f:
@@ -9,3 +12,14 @@ def load_e2e_config(filepath):
         w['id'] = i + 1
         config['gw%d' % i] = w
     return config
+
+def docker_compose_up(config):
+    for name, c in config.items():
+        env = os.environ.copy()
+        env['ID'] = str(c['id'])
+        env['WEB_PORT'] = str(c['web_port'])
+        subprocess.run(['docker-compose', '-p', name, 'up', '-d'], env=env)
+
+def docker_compose_down(config):
+    for name, c in config.items():
+        subprocess.run(['docker-compose', '-p', name, 'down'])
